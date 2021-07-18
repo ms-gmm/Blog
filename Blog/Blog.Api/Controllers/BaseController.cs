8825aaaa1ee92e2ef5076557f3c6b94949c22413
gmm <gmm@wdora.com>
2021-07-18 20:39:50 +0800

    23

---
+++ Blog/Blog.Api/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Api.Controller
+{
+    [ApiController]
+    [Route("api/[action]/controller")]
+    public class BaseController : ControllerBase
+    {
+       
+    }
+}
---
+++ Blog/Blog.Api/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Api.Controller
+{
+    public class HomeController : BaseController
+    {
+        [HttpGet]
+        public ActionResult<string> Test()
+        {
+            return "hello world";
+        }
+    }
+}
